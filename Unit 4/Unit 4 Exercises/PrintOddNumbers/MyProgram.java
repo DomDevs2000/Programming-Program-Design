95d1993b85a08ed69910908b2dc9238f0e81e0be
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-10-11 21:28:42 +0100

    completed task 4

--- Unit 4/Unit 4 Exercises/PrintOddNumbers/MyProgram.java
+++ Unit 4/Unit 4 Exercises/PrintOddNumbers/MyProgram.java
@@ -1,8 +1,10 @@
 
 public class MyProgram {
     public static void main(String[] args) {
-        for (int i = 0; i <= 20; i++) {
-            System.out.println(i);
+        for (int i = 0; i <= 50; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
         }
 
     }
