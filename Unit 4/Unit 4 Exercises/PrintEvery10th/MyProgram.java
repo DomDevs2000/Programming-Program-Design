1bd60b8de29c2a178aa0e4149585311e52e3f7ca
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-10-11 21:32:21 +0100

    completed task 5

---
+++ Unit 4/Unit 4 Exercises/PrintEvery10th/MyProgram.java
@@ -0,0 +1,11 @@
+
+public class MyProgram {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 10 == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
