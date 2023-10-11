ac05be7570347f76acf56055194d0bbfa3e4cd65
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-10-11 21:25:42 +0100

    completed task 3 exercise

---
+++ Unit 4/Unit 4 Exercises/PrintLastNumber/MyProgram.java
@@ -0,0 +1,10 @@
+
+public class MyProgram {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i == 100) {
+                System.out.println(i);
+            }
+        }
+    }
+}
