8e3d54f5c026c89c6a93a31651537e6df397768c
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-11-21 11:07:54 +0000

    completed stocklist exercise

---
+++ ticked-exercises/Unit13/stocklist/TestStockList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class TestStockList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TestStockList {
+
+    public static void main(String[] args) {
+
+        StockList warehouse = new StockList();
+
+        warehouse.addNewStockItem("Product A", 5);
+        warehouse.addNewStockItem("Product B", 50);
+        warehouse.addNewStockItem("Product C", 10);
+        warehouse.addNewStockItem("Product D", 40);
+        warehouse.addNewStockItem("Product E", 70);
+
+        System.out.println("Number of stock items in the warehouse: " + warehouse.noOfStockItems());
+
+        warehouse.showItem(0);
+
+        warehouse.noOfStockItems();
+
+        warehouse.updateStockLevel(0, 5);
+
+        warehouse.updateStockLevel(1, 10);
+
+        warehouse.removeItem(2);
+
+        warehouse.showItem(0);
+
+    }
+}
