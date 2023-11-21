714652c72b5dfe8d04f2d2f1f9cf7af57356f7d6
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-11-21 11:08:15 +0000

    updated code for task

---
+++ ticked-exercises/Unit14/hospital/AEUsersList.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+/**
+ * AEArrayList
+ */
+public class AEUsersList {
+
+    private ArrayList<Patient> users = new ArrayList<Patient>();
+
+    public void addPatient(Patient p) {
+        users.add(p);
+    }
+
+    public void addPatient(int pN, String nme, String gp, int d, int m, int y) {
+        Patient patient = new Patient(pN, nme, gp, d, m, y);
+        users.add(patient);
+    }
+
+    public void addPatient(int index, Patient p) {
+        users.add(index, p);
+    }
+
+    public void removePatient(Patient p) {
+        users.remove(p);
+    }
+
+    public int findPatientByPNo(int pNo) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getPatientNo() == pNo) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Patient getPatient(int pos) {
+        return users.get(pos);
+    }
+
+    public Patient getPatientByPNo(int pNo) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getPatientNo() == pNo) {
+                Patient patientByNo = users.get(i);
+                return patientByNo;
+            }
+        }
+        return null;
+    }
+
+    public void updatedAll() {
+        for (Patient patient : users) {
+            patient.updateWaitingTime(1);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "AEUsersList [users=" + users + "]";
+    }
+
+}
---
+++ ticked-exercises/Unit14/hospital/AEUsersMap.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class AEUsersMap {
+    private Map<Integer, Patient> users = new HashMap<>();
+
+    public void addPatient(Patient p) {
+        users.put(1, p);
+    }
+
+    public void addPatient(int pNo, String nme, String gp, int d, int m, int y) {
+        Patient patient = new Patient(pNo, nme, gp, d, m, y);
+        users.put(pNo, patient);
+    }
+
+    public void addPatient(int index, Patient patient) {
+        users.put(patient.getPatientNo(), patient);
+    }
+
+    public void removePatient(int pNo) {
+        users.remove(pNo);
+    }
+
+    public int findPatientByPNo(int pNo) {
+        if (users.containsKey(pNo)) {
+            return pNo;
+        } else
+            return -1;
+    }
+
+    public Patient getPatient(int pNo) {
+        return users.get(pNo);
+    }
+
+    public Patient getPatientByPNo(int pNo) {
+        return users.get(pNo);
+    }
+
+    public void updateAll() {
+        for (Patient patient : users.values()) {
+            patient.updateWaitingTime(1);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "AEUsersMap []";
+    }
+}
