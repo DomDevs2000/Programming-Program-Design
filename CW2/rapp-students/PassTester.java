eff7c547543437cdec4bf1a0e0d83bad607e684b
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-11-25 00:34:08 +0000

    first implementation of pass tester class

--- CW2/rapp-students/PassTester.java
+++ CW2/rapp-students/PassTester.java
@@ -1,20 +1,17 @@
- 
 
-  
 /**
  * Write a description of class PassTester here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class PassTester
-{
-    public static void main(String[] args)
-    {
-        //Write your tests here
-        
-        
+public class PassTester {
+    public static void main(String[] args) {
+        Pass pass = new Pass("Aidan", 10, 10, 10, 10);
+        int passId = pass.getPassID();
+
+        System.out.println(passId);
+
     }
-        
-    
+
 }
