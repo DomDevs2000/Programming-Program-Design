a7148097acba0ab107523b0c707625ec2a55a299
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-11-28 13:35:31 +0000

    implemented code for bridge class

--- CW2/rapp-students/Bridge.java
+++ CW2/rapp-students/Bridge.java
@@ -1,15 +1,46 @@
- 
 
-  
 /**
  * A bridge provides a one-way connection between two zones. It
- has a bridge code and information about both the source and
- the destination zone
+ * has a bridge code and information about both the source and
+ * the destination zone
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Bridge
-{
-    
+public class Bridge {
+    private int bridgeCode;
+    private Zone sourceZone;
+    private Zone destinationZone;
+
+    public Bridge(int bridgeCode, Zone sourceZone, Zone destinationZone) {
+        this.bridgeCode = bridgeCode;
+        this.sourceZone = new Zone("Lobby", 0, 0, 1000, 1000);
+        this.destinationZone = new Zone("Concorse", 1, 1, 100, 10);
+
+    }
+
+    public int getBridgeCode() {
+        return bridgeCode;
+    }
+
+    public Zone getSourceZone() {
+        return sourceZone;
+    }
+
+    public Zone getDestinationZone() {
+        return destinationZone;
+    }
+
+    public boolean canUseBridge(Pass pass) {
+        if (destinationZone.noOfPeople < destinationZone.capacity) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "Bridge [sourceZone=" + sourceZone + ", destinationZone=" + destinationZone + "]";
+    }
+
 }
