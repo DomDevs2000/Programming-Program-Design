ca62256d839ab473cc155e0ed9a783fc0303bbb5
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-11-30 12:42:57 +0000

    updated code for task 3

--- CW2/rapp-students/Park.java
+++ CW2/rapp-students/Park.java
@@ -1,184 +1,208 @@
- 
-
-   
 
 import java.util.*;
 
-/**This class implements the RAPP interface
+/**
+ * This class implements the RAPP interface
  *
- * @author A.A.Marczyk 
+ * @author A.A.Marczyk
  * @version 15/10/23
  **/
-public class Park implements RAPP
-{
+public class Park implements RAPP {
     private String parkName;
-    private ArrayList<Pass> allPasses= new ArrayList<Pass>();
+    private ArrayList<Pass> allPasses = new ArrayList<Pass>();
+    private ArrayList<Zone> zones = new ArrayList<Zone>();
+    private ArrayList<Bridge> bridges = new ArrayList<Bridge>();
     // complete this section by adding other required fields
 
-    
-    
-    
-    /** constructor
+    /**
+     * constructor
+     * 
      * @param nme - name of park
      */
-    public Park(String nme) 
-    {
-     
-        
+    public Park(String nme) {
+        this.parkName = nme;
+        loadZones();
+        loadPasses();
+        setUpBridges();
     }
-    
+
     /**
-     * Returns all of the details of the park: its name and its zones 
+     * Returns all of the details of the park: its name and its zones
      * including lists of passes in each zone.
      */
-    public String toString()
-    {
-         
+    public String toString() {
+
         return "";
     }
-    
-    /**Returns a String with details of a pass
+
+    /**
+     * Returns a String with details of a pass
+     * 
      * @param passId - id number of the pass
      * @return the details of the pass as a String, or "No such pass"
      */
-    public String getPassDetails(int passId)
-    {
-        
+    public String getPassDetails(int passId) {
+
         return "";
-    } 
-    
-    /**Returns the name of the zone which contains the specified pass or null
+    }
+
+    /**
+     * Returns the name of the zone which contains the specified pass or null
+     * 
      * @param ccd - the specified pass
      * @return the name of the Zone which contains the pass, or null
      **/
-    public String getPassLocation(int cd)
-    {
-        
+    public String getPassLocation(int cd) {
+
         return null;
     }
-    
-    /** Given the name of a zone, returns the zone id number
+
+    /**
+     * Given the name of a zone, returns the zone id number
      * or -1 if zone does not exist
+     * 
      * @param name of zone
      * @return id number of zone
      */
-    public int getZoneNumber(String ww)
-    {
-        
+    public int getZoneNumber(String ww) {
+
         return 0;
     }
-                
-    /**Returns a String representation of all the passes on specified zone
+
+    /**
+     * Returns a String representation of all the passes on specified zone
+     * 
      * @param zone is the name of the zone
      * @return a String representation of all passes on specified zone
      **/
-    public String getAllPassesInOneZone(String zone)
-    {
-        
-        return "";
-    } 
+    public String getAllPassesInOneZone(String zone) {
 
-    
-    /**Returns true if a Pass is allowed to move using the bridge, false otherwise
-     * A move can be made if:  
-     * the rating of the pass  >= the rating of the destination zone
+        return "";
+    }
+
+    /**
+     * Returns true if a Pass is allowed to move using the bridge, false otherwise
+     * A move can be made if:
+     * the rating of the pass >= the rating of the destination zone
      * AND the destination zone is not full
      * AND the pass has sufficient credits
      * AND the pass is currently in the source zone
      * AND the pass id is for a pass on the system
      * AND the bridge code is the code for a bridge on the system
-     * @param trId is the id of the pass requesting the move
+     * 
+     * @param trId   is the id of the pass requesting the move
      * @param znCode is the code of the bridge by which the pass wants to move
-     * @return true if the pass is allowed on the bridge journey, false otherwise 
-    **/
-    public boolean canMove(int trId, String znCode)
-    {   
-        
-        return false;
-    }     
+     * @return true if the pass is allowed on the bridge journey, false otherwise
+     **/
+    public boolean canMove(int trId, String znCode) {
 
-    /**Returns the result of a pass requesting to move over a bridge.
-     * A move will be successful if:  
-     * the luxury rating of the pass  >= the luxury rating of the destination zone
+        return false;
+    }
+
+    /**
+     * Returns the result of a pass requesting to move over a bridge.
+     * A move will be successful if:
+     * the luxury rating of the pass >= the luxury rating of the destination zone
      * AND the destination zone is not full
      * AND the pass has sufficient credits
      * AND the pass is currently in the source zone
      * AND the pass id is for a pass in the system
      * AND the bridge code is the code for a bridge on the system
-     * If the bridge crossing can be made, the pass information is removed from 
-     * the source zone, added to the destination zone, pass details updated and a suitable message returned.
+     * If the bridge crossing can be made, the pass information is removed from
+     * the source zone, added to the destination zone, pass details updated and a
+     * suitable message returned.
      * a suitable message returned.
      * If bridge journey cannot be made, the state of the system remains unchanged
      * and a message specifying the reason is returned.
+     * 
      * @param pPassId is the id of the pass requesting the move
-     * @param znCode is the code of the bridge by which the pass wants to move
-     * @return a String giving the result of the request 
+     * @param znCode  is the code of the bridge by which the pass wants to move
+     * @return a String giving the result of the request
      **/
-    public String move(int pPassId, String znCode )
-    {   
+    public String move(int pPassId, String znCode) {
         return "";
-    }     
-
-    /** Allows a pass to top up their credits.This method is not concerned with 
-     *  the cost of a credit as currency and prices may vary between resorts.
-     *  @param id the id of the pass toping up their credits
-     *  @param creds the number of credits purchased to be added to passes information
-     */
-    public void topUpCredits(int id, int creds)
-    {
-        
     }
 
-   /** Allows a pass to convert its points to credits
-     *  @param cdId the id of the pass converting their credits
+    /**
+     * Allows a pass to top up their credits.This method is not concerned with
+     * the cost of a credit as currency and prices may vary between resorts.
+     * 
+     * @param id    the id of the pass toping up their credits
+     * @param creds the number of credits purchased to be added to passes
+     *              information
      */
-    public void convertPoints(int cdId)
-    {
-    
+    public void topUpCredits(int id, int creds) {
+
     }
-    //***************private methods**************
-    private void loadZones()
-    {
-        
+
+    /**
+     * Allows a pass to convert its points to credits
+     * 
+     * @param cdId the id of the pass converting their credits
+     */
+    public void convertPoints(int cdId) {
+
     }
-    
-    private void setUpBridges()
-    {
-        
+
+    // ***************private methods**************
+    private void loadZones() {
+        zones.add(new Zone("Lobby", 0, 0, 1000, 10));
+        zones.add(new Zone("Concorse", 1, 3, 100, 50));
+        zones.add(new Zone("WaterWorld", 2, 3, 10, 3));
+        zones.add(new Zone("WildWest", 3, 5, 2, 0));
+        zones.add(new Zone("Solitaire", 4, 1, 1, 1));
+
     }
-    
-    private void loadPasses()
-    {
-        
+
+    private void setUpBridges() {
+        bridges.add(new Bridge("ABC1", zones.get(0), zones.get(1)));
+        bridges.add(new Bridge("BCD2", zones.get(1), zones.get(0)));
+        bridges.add(new Bridge("CDE3", zones.get(1), zones.get(2)));
+        bridges.add(new Bridge("DEF4", zones.get(2), zones.get(1)));
+        bridges.add(new Bridge("JKL8", zones.get(2), zones.get(3)));
+        bridges.add(new Bridge("EFG5", zones.get(3), zones.get(1)));
+        bridges.add(new Bridge("GHJ6", zones.get(1), zones.get(4)));
+        bridges.add(new Bridge("HJK7", zones.get(4), zones.get(1)));
+
+        // bridges.add(new Bridge("ABC1", new Zone("Lobby", 0, 0, 1000, 10), new
+        // Zone("Concorse", 1, 3, 100, 50)));
+        // bridges.add(new Bridge("BCD2", new Zone("Lobby", 0, 0, 1000, 10), new
+        // Zone("Concorse", 1, 3, 100, 50)));
+
     }
- 
-    /** Returns the pass with the pass id specified by the parameter
+
+    private void loadPasses() {
+        allPasses.add(new Pass("John", 1, 3, 5));
+
+    }
+
+    /**
+     * Returns the pass with the pass id specified by the parameter
+     * 
      * @return the pass with the specified id
      **/
-    private Pass getPass(int id)
-    {
-        
+    private Pass getPass(int id) {
+
         return null;
     }
-    
-    
-    /** Returns the zone with the name specified by the parameter
+
+    /**
+     * Returns the zone with the name specified by the parameter
+     * 
      * @return the zone with the specified name
      **/
-    private Zone getZone(String zoneName)
-    {
-        
+    private Zone getZone(String zoneName) {
+
         return null;
     }
-    
-    /** Returns the zone with the name specified by the parameter
+
+    /**
+     * Returns the zone with the name specified by the parameter
+     * 
      * @return the zone with the specified name
      **/
-    private Bridge getBridge(String br)
-    {
-       return null; 
+    private Bridge getBridge(String br) {
+        return null;
     }
-    
-    
-    
+
 }
