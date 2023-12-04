1a22dcb81d703ad44aa3771a8648f3e038a690e5
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-12-04 10:01:03 +0000

    moved zone tester to mytester class

--- CW2/rapp-students/MyTester.java
+++ CW2/rapp-students/MyTester.java
@@ -1,28 +1,32 @@
 import java.util.*;
+
 /**
  * Write a description of class MyTester here.
  * 
- * @author 
- * @version 
+ * @author
+ * @version
  */
-public class MyTester 
-{   
-    
-    private void doTest()
-    {
+public class MyTester {
+
+    private void doTest() {
         RAPP fantasia = new Park("Fantasia Pleasure Park");
         Scanner enter = new Scanner(System.in);
-        // write your tests here
-        
-        
-        
 
-     }
-     
-     
-    
-    public static void main(String[] args)
-    {
+        // Zone obj tests
+        Zone zone = new Zone("1", 123, 3, 10, 10);
+        Pass pass = new Pass("aidan", 123, 3, 10);
+        zone.getZoneNumber();
+        zone.getName();
+        zone.getLuxuryRating();
+        zone.toString();
+        zone.enter(pass);
+        System.out.println(zone.listAllPasses());
+        System.out.println(zone.isPassInZone(pass));
+        System.out.println(zone.toString());
+
+    }
+
+    public static void main(String[] args) {
         MyTester xx = new MyTester();
         xx.doTest();
     }
