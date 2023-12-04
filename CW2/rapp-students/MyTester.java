5f26ab232dbcd91de61e78018c6d9da540e5315d
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-12-04 13:21:52 +0000

    added methods to tester class

--- CW2/rapp-students/MyTester.java
+++ CW2/rapp-students/MyTester.java
@@ -15,13 +15,19 @@ public class MyTester {
         // Zone obj tests
         Zone zone = new Zone("1", 123, 3, 10, 10);
         Pass pass = new Pass("aidan", 123, 3, 10);
+        Pass pass2 = new Pass("Dom", 321, 3, 10);
         zone.getZoneNumber();
         zone.getName();
         zone.getLuxuryRating();
         zone.toString();
         zone.enter(pass);
+        zone.enter(pass2);
+        System.out.println(zone.findPassDetails(123));
+        System.out.println(zone.findPassDetails(321));
+
         System.out.println(zone.listAllPasses());
         System.out.println(zone.isPassInZone(pass));
+        System.out.println(zone.isPassInZone(pass2));
         System.out.println(zone.toString());
 
     }
