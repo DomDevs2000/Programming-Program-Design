d57a98134c739b84c0a8400c8132798d139c5f4a
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-12-08 19:48:58 +0000

    added SRN and name

--- CW2/rapp-students/Teamwork.java
+++ CW2/rapp-students/Teamwork.java
@@ -1,45 +1,35 @@
- 
-
-  
-
 
 /**
  * Details of your team
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Teamwork
-{
+public class Teamwork {
     private String[] details = new String[6];
-    
-    public Teamwork()
-    {   // In each line replace the contents of the String 
-        // with the details of your team
-        // It will help us if the surname of programmer1 comes
-        // alphabetically before the surname of programmer2
-        // If there is only 1 team member, please complete details 
-        // for programmer1
-        
-        details[0] = "surname of programmer1";
-        details[1] = "first name of programmer1";
-        details[2] = "SRN of programmer1";
+
+    public Teamwork() { // In each line replace the contents of the String
+                        // with the details of your team
+                        // It will help us if the surname of programmer1 comes
+                        // alphabetically before the surname of programmer2
+                        // If there is only 1 team member, please complete details
+                        // for programmer1
+
+        details[0] = "Carvalho";
+        details[1] = "Aidan";
+        details[2] = "17045629";
         details[3] = "surname of programmer2";
         details[4] = "first name of programmer2";
         details[5] = "SRN of programmer2";
     }
-    
-    public String[] getTeamDetails()
-    {
+
+    public String[] getTeamDetails() {
         return details;
     }
-    
-    public void displayDetails()
-    {
-        for(String temp:details)
-        {
+
+    public void displayDetails() {
+        for (String temp : details) {
             System.out.println(temp.toString());
         }
     }
 }
-        
