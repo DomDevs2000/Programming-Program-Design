1b09e78e9e8dee35edbb4b1568686f4857b84afb
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-12-08 18:37:53 +0000

    updated pass tester

--- CW2/rapp-students/PassTester.java
+++ CW2/rapp-students/PassTester.java
@@ -13,15 +13,18 @@ public class PassTester {
         int passId = pass.getPassID();
         int luxuryRating = pass.getLuxuryRating();
         int credits = pass.getNoOfCredits();
+        String passAsString = pass.toString();
         // print result
 
-        // NOTE: flip variables and string
-        System.out.println(passId + " expect passID of 10");
-        System.out.println(luxuryRating + " expect luxury rating of 5");
-        System.out.println(credits + " expect credits of 15");
-        System.out.println(pass.toString());
+        System.out.println("Expect passID of 10 - result is: " + passId);
+        System.out.println("Expect luxury rating of 5 - result is: " + luxuryRating);
+        System.out.println("Expect credits to be 15 -  result is:  " + credits);
+        System.out.println(
+                "Expect pass object with name: Aidan, passId: 10, luxuryRating: 5, noOfcredits: 15, noOfPoints: 0 - \n result is: "
+                        + passAsString);
+
         pass.deductCredits();
-        System.out.println(pass.getNoOfCredits() + " expect credits of 11 after travel ");
+        System.out.println("Expect credits of 11 after travel - result is:  " + pass.getNoOfCredits());
     }
 
 }
