3ba81eeebfaa657e7d98786f68c49db6e66a4e34
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-12-10 00:07:47 +0000

    removed comments

--- CW2/rapp-students/Zone.java
+++ CW2/rapp-students/Zone.java
@@ -54,13 +54,6 @@ public class Zone {
             passes.remove(position);
         }
     }
-    // NOTE: example implemntation
-    // public void leave(int passId) {
-    // Pass position = findPassDetails(passId);
-    // if (position != null) {
-    // passes.remove(position);
-    // }
-    // }
 
     public boolean isFull() {
         if (passes.size() >= capacity) {
